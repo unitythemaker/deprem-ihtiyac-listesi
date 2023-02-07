663daea2217533b267ce65c243adb45a0c2cd154
Halil Tezcan KARABULUT <unity@themaker.cyou>
2023-02-07 22:53:06 +0300

    switch adapter to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,15 +9,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      // fallback: null,
-      precompress: false,
-      strict: false,
-    })
+		adapter: adapter()
 	},
 	prerender: {
 		default: true
